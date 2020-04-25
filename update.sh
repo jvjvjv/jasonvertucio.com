10c9a66b14e24234064af3d8ab33de8f6e06e4bf
Jason Vertucio <me@jasonvertucio.com>
2020-04-25 15:38:30 +0000

    Typo

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ git pull
 # Update Laravel dependencies
 composer install
 php artisan migrate --force
-php artisn db:seed --force
+php artisan db:seed --force
 
 # Update front-end dependencies
 npm ci
