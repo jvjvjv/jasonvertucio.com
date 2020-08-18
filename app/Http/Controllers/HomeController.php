da5f54c0267a0e2fa2a90ab2df2fd6e00b1aa44a
Jason Vertucio <me@jasonvertucio.com>
2020-08-17 21:18:35 -0400

    fixed migration bug. also run paper harvester on page load

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Wink\WinkPost;
+use Artisan;
 
 class HomeController extends Controller
 {
   public function index()
   {
+    Artisan::call('paper:harvest',[
+      "--limit" => 2
+    ]);
     $path = public_path() . "/config.json"; // ie: /var/www/laravel/public/filename.json
     $config = json_decode(file_get_contents($path), true);
     $latest_post = WinkPost::published()->live()->orderBy('publish_date','DESC')->get()[0];
--- database/migrations/2020_08_17_235254_create_papers_table.php
+++ database/migrations/2020_08_17_235254_create_papers_table.php
@@ -16,7 +16,7 @@ class CreatePapersTable extends Migration
         Schema::create('papers', function (Blueprint $table) {
             $table->id();
             $table->string('edition_id');
-            $table->json('edition');
+            $table->text('edition');
             $table->timestamp('published_at');
             $table->timestamps();
 
