dd917d865f624d63d4162384a13c7c354bb3c529
Jason Vertucio <me@jasonvertucio.com>
2020-08-22 08:19:20 -0400

    Messed with layout and added ad code

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -5,57 +5,71 @@
 @section('main')
 <div id="fb-root"></div>
 <script async defer cookie-consent="functionality" crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0" nonce="oiR10rcH"></script>
-<div class="blog-item single">
 
-  <h2 class="title">
-    {{ $post['title'] }}
-  </h2>
-  <div class="content">
-    {!! $post['content'] !!}
-  </div>
-
-    <div class="list-meta">
-    {{-- @if ($post->author)
-      <strong>By:</strong> {{ $post->author['name'] }}
-      |
-    @endif --}}
-    <strong>Published:</strong> {{ $post['publish_date']->format('m/d/Y g:ia') }}
-    @ifwinkauthenticated
-    | <a href="/wink/posts/{{ $post['id'] }}" class="card-link">Edit</a>
-    @endifwinkauthenticated
-  </div>
-  <div class="share">
-    <span class="share-header">Share:</span>
-    <div class="social-icons px-2">
-      <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" target="_blank" class="twitter-share-button" data-show-count="false">
-        <i class="fab fa-twitter"></i>
-      </a>
-    </div>
-    <div class="social-icons px-2">
-      <div
-        class="fb-share-button"
-        data-href="{{ Request::url() }}"
-        data-lazy="true"
-        data-order-by="social"
-        data-layout="button_count"></div>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h2 class="title mt-3 mb-5">
+        {{ $post['title'] }}
+      </h2>
     </div>
   </div>
-  <div class="comments">
-    <h3>Comments</h3>
-    <div
-      class="fb-comments"
-      data-href="{{ Request::url() }}"
-      data-lazy="true"
-      data-numposts="10"
-      data-width="100%"></div>
-  </div>
-@if (env('APP_DEBUG'))
-  <pre>
-  {{-- @json($post, JSON_PRETTY_PRINT) --}}
-  {{ json_encode ($post, JSON_PRETTY_PRINT) }}
-  </pre>
-@endif
 
+  <div class="row">
+    <div class="col-sm-8">
+
+      <div class="blog-item single">
+        <div class="content">
+          {!! $post['content'] !!}
+        </div>
+        <div class="list-meta">
+          {{-- @if ($post->author)
+            <strong>By:</strong> {{ $post->author['name'] }}
+            |
+          @endif --}}
+          <strong>Published:</strong> {{ $post['publish_date']->format('m/d/Y g:ia') }}
+          @ifwinkauthenticated
+          | <a href="/wink/posts/{{ $post['id'] }}" class="card-link">Edit</a>
+          @endifwinkauthenticated
+        </div>
+        <div class="share">
+          <span class="share-header">Share:</span>
+          <div class="social-icons px-2">
+            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" target="_blank" class="twitter-share-button" data-show-count="false">
+              <i class="fab fa-twitter"></i>
+            </a>
+          </div>
+          <div class="social-icons px-2">
+            <div
+              class="fb-share-button"
+              data-href="{{ Request::url() }}"
+              data-lazy="true"
+              data-order-by="social"
+              data-layout="button_count"></div>
+          </div>
+        </div>
+        <div class="comments">
+          <h3>Comments</h3>
+          <div
+            class="fb-comments"
+            data-href="{{ Request::url() }}"
+            data-lazy="true"
+            data-numposts="10"
+            data-width="100%"></div>
+        </div>
+        @if (env('APP_DEBUG'))
+          <pre>
+          {{-- @json($post, JSON_PRETTY_PRINT) --}}
+          {{ json_encode ($post, JSON_PRETTY_PRINT) }}
+          </pre>
+        @endif
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <script data-ad-client="ca-pub-0429292532295045" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <em>This space intentionally left blank.</em>
+    </div>
+  </div>
 </div>
 
 @endsection
