05ec7a744b3439c65c74fb1f0011455bc1559ec9
Jason Vertucio <me@jasonvertucio.com>
2021-10-17 18:48:36 -0400

    Added logic to allow topics/tags

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use Canvas\Events\PostViewed;
 use Canvas\Models\Post;
+use Canvas\Models\Tag;
+use Canvas\Models\Topic;
 
 use Auth;
 
@@ -16,16 +18,43 @@ class BlogController extends Controller
   {
     $list = Post::published()->orderBy('published_at','DESC')->get();
     return view('blog.list', [
-      'list' => $list,
+        'list' => $list,
         'links' => [
-          [ 'href' => '#', 'label' => 'Posts' ]
+            [ 'href' => '#', 'label' => 'Posts' ]
         ],
     ]);
   }
 
+  public function topicsOrTags($slug) {
+    $topic = Topic::with('posts')->where('slug',$slug)->first();
+    if ($topic) {
+      return view('blog.list', [
+          'list' => $topic->posts->whereNotNull('published_at'),
+          'links' => [
+            [ 'href' => '#', 'label' => 'Posts' ]
+          ],
+        ]);
+    } else {
+      $tag = Tag::with('posts')->where('slug',$slug)->first();
+      if (!$tag) {
+        abort(404);
+      } else {
+        return view('blog.list', [
+          'list' => $tag->posts->whereNotNull('published_at'),
+          'links' => [
+            [ 'href' => '#', 'label' => 'Posts' ]
+          ],
+        ]);
+      }
+    }
+  }
+
   public function post($slug)
   {
-    $post = Post::with(['user','tags','topic'])->where('slug',$slug)->firstOrFail();
+    $post = Post::with(['user','tags','topic'])->where('slug',$slug)->first();
+    if (!$post) {
+      return $this->topicsOrTags($slug);
+    }
     $auth = Auth::guard('canvas');
     if ($auth->check() && $post->user->id === $auth->user()->id) {
       // Do nothing
