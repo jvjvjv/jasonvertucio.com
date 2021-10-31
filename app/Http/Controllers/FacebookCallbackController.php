27c24ea589f266182f086018e0efcbe7330acd6b
Jason Vertucio <me@jasonvertucio.com>
2021-10-31 12:53:36 -0400

    Fixed webhook

--- app/Http/Controllers/FacebookCallbackController.php
+++ app/Http/Controllers/FacebookCallbackController.php
@@ -8,7 +8,15 @@ use Log;
 class FacebookCallbackController extends Controller
 {
     public function index(Request $request) {
+        if ($request->hub_verify_token && $request->hub_verify_token !== 'jasonvertucioisamotherfuckingdog') {
+            return response(null,401);
+        }
         Log::info($request->method(), $request->input());
+
+        if ($request->hub_mode === "subscribe") {
+            return response($request->hub_challenge, 200);
+        }
+
         return response([
             'method' => $request->method(),
             'data' => $request->input()
