13242877420a6547d6bc088f143a7282630fee39
Jason Vertucio <me@jasonvertucio.com>
2023-01-05 17:40:03 -0500

    Just adding these things ....?

---
+++ app/Services/TwilioService.php
@@ -0,0 +1,91 @@
+<?php namespace App\Services;
+
+use Twilio\Rest\Client;
+use Log;
+
+class TwilioService {
+  private $sid;
+  private $token;
+  private $client;
+
+  public function __construct() {
+    $this->sid = env("TWILIO_SID");
+    $this->token = env("TWILIO_TOKEN");
+    $this->fromPhone = env("TWILIO_FROM");
+
+    $this->client = new Client($this->sid, $this->token);
+  }
+
+  /**
+   * sendMessage
+   * Sends a message to the specified recipient
+   *
+   * @param string $message The message to send (160-character max)
+   * @param string $recipient The recipient's phone number
+   * @param string $sender optional, send from a different phone number than the one specified in the .env file
+   *
+   * @return obj Message object
+   */
+  public function sendMessage($message, $recipient, $sender = null) {
+    if ($sender == null) {
+      $sender = $this->fromPhone;
+    }
+    $start = microtime(true);
+    $message = $this->client->messages->create($recipient, [
+      "from" => $sender,
+      "body" => $message
+    ]);
+
+    $end = microtime(true);
+    Log::info("API: Message to " . $recipient . " took " . ($end - $start) . "ms");
+
+    return $message;
+  }
+
+  /**
+   * sendMessageWithCallback
+   * Sends a message to the specified recipient
+   *
+   * @param string $message The message to send (160-character max)
+   * @param string $recipient The recipient's phone number
+   * @param string $sender optional, send from a different phone number than the one specified in the .env file
+   *
+   * @return obj Message object
+   */
+  public function sendMessageWithCallback($message, $recipient, $sender = null) {
+    if ($sender == null) {
+      $sender = $this->fromPhone;
+    }
+    $start = microtime(true);
+    $message = $this->client->messages->create($recipient, [
+      "from" => $sender,
+      "body" => $message,
+      "statusCallback" => env('APP_URL') . "/sms/callback"
+    ]);
+
+    $end = microtime(true);
+    Log::info("API: Message to " . $recipient . " took " . ($end - $start) . "ms");
+
+    return $message;
+  }
+
+  /**
+   * getMessageStatus
+   *
+   * @param string $sid
+   *
+   * @return obj status
+   */
+  public function getMessage($sid) {
+    try {
+      return $this->client->messages($sid)->fetch();
+    } catch (\Twilio\Exceptions\RestException $e) {
+      if ($e->getStatusCode() === 404) {
+        Log::notice("TwilioService::getMessage({$sid}) Exception " . $e->getStatusCode() .": " . $e->getMessage() . " (" . $e->getCode() . ")");
+      } else {
+        Log::alert("TwilioService::getMessage({$sid}) Exception " . $e->getStatusCode() .": " . $e->getMessage() . " (" . $e->getCode() . ")");
+      }
+      return $e;
+    }
+  }
+}
---
+++ database/migrations/2020_06_06_203125_create_phone_messages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhoneMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phone_messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('message');
+            $table->string('sid',64);
+            $table->unsignedBigInteger('from_phone_id');
+            $table->unsignedBigInteger('to_phone_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('from_phone_id')->references('id')->on('phone_numbers');
+            $table->foreign('to_phone_id')->references('id')->on('phone_numbers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phone_messages');
+    }
+}
