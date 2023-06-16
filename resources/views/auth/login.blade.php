eb27ece7dcc26e845dfb694f87c469baa887bd7f
Jason Vertucio <me@jasonvertucio.com>
2023-06-16 06:52:10 -0400

    updated to laravel 9

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,6 +26,7 @@
                         </div>
 
                         <div class="form-group row">
+                            @csrf
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
