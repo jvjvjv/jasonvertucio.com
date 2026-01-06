b8f722a29d0f485760a18c4b407a42748e48087a
Jason Vertucio <m.jasonvertucio@gmail.com>
2026-01-06 09:56:53 -0500

    Update project and allow HTML output

--- resources/views/components/project.blade.php
+++ resources/views/components/project.blade.php
@@ -1,4 +1,4 @@
 <div class="mt-2">
     <h4 class="font-heading text-xl mb-2 font-bold">{{ $project['title'] }}</h4>
-    <p>{{ $project['description'] }}</p>
+    {!! $project['description'] !!}
 </div>
