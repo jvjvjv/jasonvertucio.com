fe6c1575f32cac110b1630ed5b2a1d0fb66a343e
Jason Vertucio <m.jasonvertucio@gmail.com>
2026-01-24 21:09:14 -0500

    add heading font

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -90,7 +90,7 @@ if (isset($post) && isset($post['meta'])) {
                 <div class="max-w-7xl mx-auto px-4">
                     <div class="flex justify-between items-center h-16">
                         <div class="flex items-center">
-                            <a class="text-white text-xl" href="{{ route('home') }}">Jason Vertucio</a>
+                            <a class="text-white text-xl font-heading" href="{{ route('home') }}">Jason Vertucio</a>
                             <ul class="hidden md:flex ml-10 space-x-4">
                                 @foreach($links as $link)
                                     <li>
