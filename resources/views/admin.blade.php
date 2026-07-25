de09c135092fed114533f3ef6c3fbb6c0ffb22cc
Jason Vertucio <m.jasonvertucio@gmail.com>
2026-07-25 14:47:54 -0400

    refactor: remove title tag from admin blade template

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,9 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>Admin | Jason Vertucio</title>
-
     {{-- Fonts --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
